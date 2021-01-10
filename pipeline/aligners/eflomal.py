b0a0813e4c14e5f95ee5e9013dc048cee700a1da
Ondřej Měkota <ondrej.mekota@me.com>
2021-01-10 18:04:23 +0100

    fixing

--- pipeline/aligners/eflomal.py
+++ pipeline/aligners/eflomal.py
@@ -29,10 +29,11 @@ class WordAligner:
             align:
 
         Returns:
-            tuple of str and tuple
+            tuple of str, tuple of lists of int,  and tuple
             -   str, each line represents aligned segment,
                 tokens are space separated, there is '|||' on each line,
                 separating the two texts.
+            -   tuple, lists of 'src' and 'tgt' aligned sentece indices
             -   tuple, contains lists for each text with original sentence endings
                 e.g. ([[4,9],...],[...]) = first text's first sentence's tokens are 0,1,2,3, and its second sentence's tokens are 0..8.
 
@@ -54,13 +55,21 @@ class WordAligner:
 
         root = etree.parse(align).getroot()
         for link in root:
+            it = 0
             for i, ind_text in enumerate(link.get('xtargets').split(';')):
                 indices[i].append([int(a) for a in ind_text.strip().split(' ') if a != ''])
+                it += 1
+            for i in range(it):
+                if len(indices[i]) == 0:  # if there is an unaligned sentece
+                    for j in range(it):  # remove all such links
+                        indices[j].pop(-1)
+                    break
 
         output = []
         for os1, os2 in zip(*indices):
             line = []
             if len(os1) == 0 or len(os2) == 0:
+                raise ValueError('INTERNAL error: An unaligned sentece has appeared. This should never happen.')
                 continue
             for i, ind_list in enumerate([os1, os2]):
                 orig_ends = []
@@ -70,19 +79,47 @@ class WordAligner:
                     orig_ends.append(curr_id + len(sents[i][l]))
                 original_sents_end[i].append(orig_ends)
                 line.append('|||')  # separator for eflomal
+
+            # TODO this will cause errors because original_sents_end is not the same length as indices
+            # TODO indices has to correspond to original_sents_end and lines in the output !!!!!!!!!
+
+
             if line[-1] == '|||':
                 line.pop(-1)
             output.append(' '.join(line))
-        return '\n'.join(output), original_sents_end
+        return '\n'.join(output), indices, original_sents_end
+
+    def _convert_eflomal_output(self, links, sentence_indices, original_sents_end):
+        """ Convert eflomal output to XML file
+
+        Args:
+            links: list of links per aligned sentences [['0-1', '3-2'], ...]
+            sentence_indices: 
+            original_sents_end: number of words per each sentence
+
+        """
+        # TODO iterate zip(sentence_indices), IGNORE such where at least of the lists is len()==0
+
+        # TODO  test last commit
+
 
 
     def align_files(self, f1_tokenized, f2_tokenized, sent_alignment):
-        # return links
+        """ Word align files
 
+        Args:
+            f1_tokenized: XML tokenized file
+            f2_tokenized: XML tokenized file
+            sent_alignment: XML alignment
+
+        Returns:
+            
+
+        """
         try:
             fd0, in_path = tempfile.mkstemp()
             fd1, out_path = tempfile.mkstemp()
-            text, original_sents_end = self._get_sents_from_xml(f1_tokenized, f2_tokenized, sent_alignment)
+            text, sent_indices, original_sents_end = self._get_sents_from_xml(f1_tokenized, f2_tokenized, sent_alignment)
             with open(fd0, 'w') as f:
                 f.write(text)
 
@@ -92,6 +129,9 @@ class WordAligner:
                 capture_output=True,
                 encoding='utf-8',
             )
+            with open(fd1) as f:
+                links = [l.strip().split(' ') for l in f.readlines()]
+            self._convert_eflomal_output(links, sentence_indices, original_sents_end)
 
         finally:
             os.remove(in_path)
